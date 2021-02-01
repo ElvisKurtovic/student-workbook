d0734c664810b5e4710538845dcb92634f801302
ElvisKurtovic <elvis.kurtovic23@gmail.com>
2021-02-01 15:51:54 -0700

    added very first blog post

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const { config } = require("vuepress-theme-hope");
 
 
-const GITHUB_USERNAME = 'REPLACE_ME' // TODO 
+const GITHUB_USERNAME = 'elviskurtovic' // TODO 
 
 
 module.exports = config({
